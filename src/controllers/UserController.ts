493d941d34efa3257da41a462e04d89e814a2ee5
luminuszz <luminuszz43@gmail.com>
2020-03-26 16:36:40 -0300

    post,upatade and delete

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,17 +2,51 @@ import { Request, Response } from 'express'
 
 import User from '../entity/User'
 class UserController {
-  async index (req: Request, res: Response):Promise<Response> {
-    const user = await User.find()
-    return res.json(user)
+  async index (req: Request, res: Response): Promise<Response> {
+    try {
+      const user = await User.find()
+      return res.send().json(user)
+    } catch (error) {
+      return res.json({ error: error.message })
+    }
   }
 
-  async store (req: Request, res: Response):Promise<Response> {
-    const data = req.body
-    console.log(data)
-    const user = await User.save(data)
+  async store (req: Request, res: Response): Promise<Response> {
+    try {
+      const data = req.body
+      const { email } = data
 
-    return res.json(user)
+      if (await User.findOne({ email })) {
+        return res.status(400).json({ messege: 'Usuário ja existe' })
+      }
+      const user = await User.save(data)
+      return res.send().json(user)
+    } catch (error) {
+      return res.json({ error: error.message })
+    }
+  }
+
+  async uptade (req: Request, res: Response): Promise<Response> {
+    try {
+      const data = req.body
+      const { id } = req.params
+      const user = await User.findOne(id)
+      User.merge(user, data)
+      const save = User.save(user)
+      return res.send().json(save)
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
+  }
+
+  async delete (req: Request, res: Response): Promise<Response> {
+    try {
+      const { id } = req.params
+      const user = User.delete(id)
+      return res.send().json(user)
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
   }
 }
 
--- src/routes.ts
+++ src/routes.ts
@@ -8,4 +8,8 @@ routes.get('/', UserController.index)
 // Post
 routes.post('/users', UserController.store)
 
+routes.put('/changeuser/:id', UserController.uptade)
+
+routes.delete('/deleteuser/:id', UserController.delete)
+
 export default routes
